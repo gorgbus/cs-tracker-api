f50960dd0680e9f28455aa962c849115b3a30df7
gorgbus <ondrejglet@gmail.com>
2023-12-14 22:50:51 +0100

    updated jwt decoding

--- src/jwt.rs
+++ src/jwt.rs
@@ -1,16 +1,15 @@
 use std::env;
 
 use jsonwebtoken::{decode, Algorithm, DecodingKey, Validation};
-use once_cell::sync::Lazy;
 use serde::{Deserialize, Serialize};
 
 use crate::error::{Error, Result};
 
-const KEY: Lazy<DecodingKey> =
-    Lazy::new(|| DecodingKey::from_secret(env::var("JWT_ACCESS_KEY").unwrap().as_bytes()));
-
 pub fn verify_token(token: &str) -> Result<User> {
-    let token_data = decode::<Claims>(token, &KEY, &Validation::new(Algorithm::HS256))
+    let decoding_key =
+        DecodingKey::from_rsa_pem(env::var("TOKEN_PUBLIC_KEY").unwrap().as_bytes()).unwrap();
+
+    let token_data = decode::<Claims>(token, &decoding_key, &Validation::new(Algorithm::RS256))
         .map_err(|_| Error::JwtInvalidToken)?;
 
     Ok(token_data.claims.user)
